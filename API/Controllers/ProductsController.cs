cbc492e0376b6867adde29a99817c94a33ad034e
sametakbal <sametakbal7@gmail.com>
2020-06-20 10:13:42 +0300

    sorting added

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -31,9 +31,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts(string sort)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
             var products = await _productRepo.ListAsync(spec);
             return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
         }
--- Core/Specification/ISpecification.cs
+++ Core/Specification/ISpecification.cs
@@ -6,8 +6,10 @@ namespace Core.Specification
 {
     public interface ISpecification<T>
     {
-        Expression<Func<T,bool>> Criteria{get;}
-        List<Expression<Func<T,object>>> Includes{get;}
-        
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+
     }
 }
