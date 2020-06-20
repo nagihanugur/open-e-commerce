eb611724e08cee07590c9c2ed13ecaac55c43084
sametakbal <sametakbal7@gmail.com>
2020-06-20 11:23:55 +0300

    cors support added

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,11 @@ namespace API
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
 
-           
+            services.AddCors(opt=>{
+                opt.AddPolicy("CorsPolicy",policy=>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +57,7 @@ namespace API
             app.UseStaticFiles();
 
             app.UseSwaggerDocumentation();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
