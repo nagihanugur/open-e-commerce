5d3fa50d3335123962e77e2d861636958bc32955
sametakbal <sametakbal7@gmail.com>
2020-07-03 21:58:58 +0300

    basket client side completed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialized basket');
+      }, error => {
+        console.log(error);
+      });
+    }
+   }
 
 }
--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-item',
@@ -9,9 +10,12 @@ import { IProduct } from 'src/app/shared/models/product';
 export class ProductItemComponent implements OnInit {
   @Input() product: IProduct;
 
-  constructor() { }
+  constructor(private basketSevice: BasketService) { }
 
   ngOnInit(): void {
   }
 
+  addItemToBasket() {
+    this.basketSevice.addItemToBasket(this.product);
+  }
 }
