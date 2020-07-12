c5b7c67084cfa0e5511530bdaac0b963ce9e0874
sametakbal <sametakbal7@gmail.com>
2020-07-12 17:08:39 +0300

    api identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.Middleware;
 using AutoMapper;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -30,12 +31,17 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
+            services.AddDbContext<AppIdentityDbContext>(x =>{
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
+
             services.AddSingleton<IConnectionMultiplexer>( c => {
                 var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),true);
                 return ConnectionMultiplexer.Connect(configuration);
             });
 
             services.AddApplicationServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
 
             services.AddCors(opt=>{
@@ -64,6 +70,8 @@ namespace API
 
             app.UseSwaggerDocumentation();
             app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- client/src/app/core/interceptors/loading.interceptros.ts
+++ client/src/app/core/interceptors/loading.interceptros.ts
@@ -11,7 +11,7 @@ export class LoadingInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         this.busyService.busy();
         return next.handle(req).pipe(
-            delay(800),
+            delay(500),
             finalize(() => {
                 this.busyService.idle();
             })
