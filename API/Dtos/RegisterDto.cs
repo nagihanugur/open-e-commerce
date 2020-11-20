82048b7cd7364707e90aca93c5ee368ec64a0f32
Samet Akbal <sametakbal@github.com>
2020-11-20 23:52:40 +0300

    gitignore update

--- API/Dtos/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Dtos
-{
-    public class RegisterDto
-    {
-        public string DisplayName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
