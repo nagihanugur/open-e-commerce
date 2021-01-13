deaa42865317d5907ef99c5ae00b48ec2093bc52
Samet AKBAL <sametakbal7@gmail.com>
2021-01-13 12:47:44 +0300

    IDao Create

--- api/src/main/java/com/sametakbal/api/dao/interfaces/IDao.java
+++ api/src/main/java/com/sametakbal/api/dao/interfaces/IDao.java
@@ -1,2 +1,9 @@
-package com.sametakbal.api.dao.interfaces;public interface IDao {
+package com.sametakbal.api.dao.interfaces;
+
+import java.util.UUID;
+
+public interface IDao<E> {
+    boolean save(E e);
+    boolean delete(UUID uuid);
+    boolean get();
 }
